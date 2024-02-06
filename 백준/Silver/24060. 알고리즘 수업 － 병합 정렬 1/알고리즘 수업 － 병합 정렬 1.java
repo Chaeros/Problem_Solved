44f7e4d373d993bbaccf3b8ec21c5bce7005391a
Chanho Park <chpark6851@koreatech.ac.kr>
2024-02-06 10:36:26 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 788 ms, Memory: 119916 KB -BaekjoonHub

---
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static int N,K;
+	public static int arr[];
+	public static int count = 0;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for ( int i = 0 ; i < N ; ++i ) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		merge_sort(0,N-1);
+		if ( count < K ) System.out.println("-1");
+	}
+
+	private static void merge_sort(int start, int end) {
+		if ( start == end ) return;
+		int mid = ( start + end ) / 2;
+		
+		merge_sort(start,mid);
+		merge_sort(mid+1,end);
+		
+		int temp[] = new int[end-start+1];
+		int left = start;
+		int right = mid + 1;
+		int index = 0;
+		while ( left <= mid && right <= end) {
+			temp[index++] = arr[left] <= arr[right] ? arr[left++] : arr[right++];
+		}
+		while ( left <= mid ) {
+			temp[index++] = arr[left++];
+		}
+		while(right <= end ) {
+			temp[index++] = arr[right++];
+		}
+		for ( int i = start ; i <= end ; ++i ) {
+			arr[i] = temp[i-start];
+			count++;
+			if ( count == K ) {
+				System.out.println(arr[i]);
+				return;
+			}
+		}
+	}
+}
