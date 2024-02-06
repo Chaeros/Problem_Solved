5326b0ba0c79ef6833fb8256c095086018edbeea
Chanho Park <chpark6851@koreatech.ac.kr>
2024-02-06 17:18:50 +0900

    [Gold IV] Title: 연구소, Time: 404 ms, Memory: 84444 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,116 +1,104 @@
-import java.io.*;
-import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N,M;
-    static int map[][];
-    static int resultMaxVal=0;
-    static boolean visited[];
-    static int wall[] = new int[3];
-    static Queue<Node> startQ = new LinkedList<>();
-
-    static class Node{
-        int row;
-        int col;
-
-        public Node(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
-    }
-
-    static void dfs(int index, int depth){
-
-        if(depth==3){
-            bfs();
-            return;
-        }
-
-        for(int i=index;i<N*M;++i){
-            int row = i/M;
-            int col = i%M;
-
-            if(!visited[i] && map[row][col]==0){
-                visited[i]=true;
-                wall[depth]=i;
-                dfs(i+1,depth+1);
-                visited[i]=false;
-            }
-        }
-    }
-
-    static int dx[] = {-1,0,1,0};
-    static int dy[] = {0,1,0,-1};
-
-    static void bfs(){
-        Queue<Node> q = new LinkedList<>(startQ);
-        int tempMap[][] = new int[N][M];
-        for(int i=0;i<N;++i){
-            for(int j=0;j<M;++j){
-                tempMap[i][j]=map[i][j];
-            }
-        }
-        boolean tempVisited[][] = new boolean[N][M];
-
-        for(int i=0;i<3;++i){
-            int row = wall[i]/M;
-            int col = wall[i]%M;
-            tempMap[row][col]=1;
-        }
-
-        while(!q.isEmpty()){
-            Node node = q.poll();
-            tempVisited[node.row][node.col]=true;
-
-            for(int i=0;i<4;++i){
-                int mx = node.row+dx[i];
-                int my = node.col+dy[i];
-
-                if(mx<0 || mx>=N || my<0 || my>=M) continue;
-                if(!tempVisited[mx][my] && tempMap[mx][my]==0){
-                    tempMap[mx][my]=2;
-                    q.offer(new Node(mx,my));
-                }
-            }
-        }
-        countSafeArea(tempMap);
-    }
-
-    static void countSafeArea(int testMap[][]){
-        int sum=0;
-        for(int i=0;i<N;++i){
-            for(int j=0;j<M;++j){
-                if(testMap[i][j]==0) ++sum;
-            }
-        }
-        resultMaxVal = Math.max(resultMaxVal,sum);
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        map = new int[N][M];
-        visited = new boolean[N*M];
-
-        for(int i=0;i<N;++i){
-            st = new StringTokenizer(br.readLine());
-            for(int j=0;j<M;++j){
-                map[i][j]=Integer.parseInt(st.nextToken());
-                if(map[i][j]==2) startQ.add(new Node(i,j));
-            }
-        }
-
-        dfs(0,0);
-
-        bw.write(resultMaxVal+"\n");
-        bw.flush();
-        bw.close();
-    }
+	public static int N,M;
+	public static boolean isWalled[][];
+	public static boolean isVisited[][];
+	public static int map[][];
+	public static List<int[]> virus = new ArrayList<>();
+	
+	public static int dr[] = {-1,0,1,0};
+	public static int dc[] = {0,1,0,-1};
+	
+	public static int virusCount = 0;
+	public static int minVirusCount = Integer.MAX_VALUE;
+	public static int wallCount = 3;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		map = new int[N][M];
+		isWalled = new boolean[N][M];
+		isVisited = new boolean[N][M];
+		
+		for ( int r = 0 ; r < N ; ++r ) {
+			st = new StringTokenizer(br.readLine());
+			for ( int c = 0 ; c < M ; ++c ) {
+				map[r][c] = Integer.parseInt(st.nextToken());
+				if ( map[r][c] == 2 ) virus.add(new int[] {r,c});
+				if ( map[r][c] == 1 ) ++wallCount;
+			}
+		}
+		
+		dfs(0,0);
+		
+		bw.write((N*M - wallCount - minVirusCount) + "\n");
+		bw.flush();
+		bw.close();		
+	}
+	
+	public static void dfs(int depth,int index) {
+		if ( depth == 3 ) {
+			virusCount = virus.size();
+			isVisited = new boolean[N][M];
+			bfs();
+			minVirusCount = Math.min(minVirusCount, virusCount);
+			return;
+		}
+		
+		for ( int n = index ; n < N*M ; ++n ) {
+			int r = n / M;
+			int c = n % M;
+			if ( map[r][c] == 0) {
+				map[r][c] = 1;
+				dfs(depth+1,n+1);
+				map[r][c] = 0;
+			}
+		}
+		
+	}
+	
+	public static void bfs() {
+		Queue<int[]> q = new ArrayDeque<>();
+		for ( int[] pos : virus ) {
+			q.offer(pos);
+			isVisited[pos[0]][pos[1]] = true;
+		}
+		
+		while ( !q.isEmpty() ) {
+			int size = q.size();
+			for ( int i = 0 ; i < size ; ++i ) {
+				int[] cPos = q.poll();
+				int row = cPos[0];
+				int col = cPos[1];
+				
+				for ( int j = 0 ; j < 4 ; ++j ) {
+					int mr = row + dr[j];
+					int mc = col + dc[j];
+					
+					if ( mr < 0 || mr >= N || mc < 0 || mc >= M ) continue;
+					if ( isVisited[mr][mc] ) continue;
+					if ( map[mr][mc] != 0 ) continue;
+					
+					isVisited[mr][mc] = true;
+					virusCount++;
+					q.offer(new int[] {mr,mc});
+				}
+			}
+		}
+	}
 }
