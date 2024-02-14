d83a5ac10cc423f8d9a915c3534d637e6f963f59
Chanho Park <chpark6851@koreatech.ac.kr>
2024-02-14 21:22:21 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 496 ms, Memory: 25528 KB -BaekjoonHub

--- 백준/Gold V/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold V/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -14,12 +14,10 @@ public class Main {
     public static class Egg{
         int durability;
         int weight;
-        boolean isCracked;
 
     public Egg(int durability, int weight) {
         this.durability = durability;
         this.weight = weight;
-        this.isCracked = false;
     }
 }
 
@@ -51,22 +49,18 @@ public static void dfs(int depth) {
     }
     
     Egg egg = eggs.get(depth);
-    if ( egg.isCracked ) {
+    if ( egg.durability <= 0 ) {
         dfs(depth+1);
         return;
     }
     for ( int i = 0 ; i < N ; ++i ) {
         if ( depth == i ) continue;
-        if ( !eggs.get(i).isCracked ) {
+        if ( eggs.get(i).durability > 0 ) {
             eggs.get(i).durability -= egg.weight;
             egg.durability -= eggs.get(i).weight;
-            if ( egg.durability <= 0 ) egg.isCracked = true;
-            if ( eggs.get(i).durability <= 0) eggs.get(i).isCracked = true;
             dfs(depth+1);
             eggs.get(i).durability += egg.weight;
             egg.durability += eggs.get(i).weight;
-            if ( egg.durability > 0 ) egg.isCracked = false;
-            if ( eggs.get(i).durability > 0) eggs.get(i).isCracked = false;
         }
     }
     maxCount = Math.max(maxCount, getCountCrackedEggs());
@@ -75,7 +69,7 @@ public static void dfs(int depth) {
 public static int getCountCrackedEggs() {
     int count = 0;
     for ( Egg egg : eggs ) {
-        if ( egg.isCracked ) ++count;
+        if ( egg.durability <= 0 ) ++count;
     }
     return count;
 }
