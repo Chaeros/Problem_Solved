47af11af73b07c73777e36824b25b52759631555
Chanho Park <chpark6851@koreatech.ac.kr>
2024-02-20 13:45:38 +0900

    [Silver II] Title: 섬의 개수, Time: 180 ms, Memory: 16080 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -2,37 +2,29 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	
-	public static int w,h;
-	public static int map[][];
-	public static boolean isVisited[][];
+	static int w,h;
+	static int map[][];
 	
-	public static int dx[] = {-1,-1,-1,0,1,1,1,0};
-	public static int dy[] = {-1,0,1,1,1,0,-1,-1};
+	static int dr[] = {-1,-1,-1,0,1,1,1,0};
+	static int dc[] = {-1,0,1,1,1,0,-1,-1};
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 		
-		while( true ) {
+		while ( true ) {
 			st = new StringTokenizer(br.readLine());
 			w = Integer.parseInt(st.nextToken());
 			h = Integer.parseInt(st.nextToken());
+			if ( w == 0 && h == 0 ) break;
 			
-			if ( w == 0 && h == 0 ) {
-				break;
-			}
 			map = new int[h][w];
-			isVisited = new boolean[h][w];
-			
 			for ( int r = 0 ; r < h ; ++r ) {
 				st = new StringTokenizer(br.readLine());
 				for ( int c = 0 ; c < w ; ++c ) {
@@ -43,8 +35,9 @@ public class Main {
 			int count = 0;
 			for ( int r = 0 ; r < h ; ++r ) {
 				for ( int c = 0 ; c < w ; ++c ) {
-					if ( map[r][c] == 1 && !isVisited[r][c]) {
-						bfs(r,c);
+					if ( map[r][c] == 1 ) {
+						map[r][c] = 0;
+						dfs(r,c);
 						++count;
 					}
 				}
@@ -52,27 +45,19 @@ public class Main {
 			bw.write(count+"\n");
 		}
 		bw.flush();
-		bw.close();
+		bw.close();	
 	}
 	
-	public static void bfs(int r, int c) {
-		Queue<int[]> q = new ArrayDeque<>();
-		q.offer(new int[] {r,c});
+	private static void dfs(int row, int col) {
 		
-		while(!q.isEmpty()) {
-			int[] pos = q.poll();
-			int x = pos[0], y = pos[1];
-			
-			for ( int d = 0 ; d < 8 ; ++d ) {
-				int mx = x + dx[d];
-				int my = y + dy[d];
-				
-				if ( mx < 0 || mx >= h || my < 0 || my >= w ) continue;
-				if ( isVisited[mx][my] ) continue;
-				if ( map[mx][my] == 0 ) continue;
-				isVisited[mx][my] = true;
-				q.offer(new int[] {mx,my});
-			}
+		for ( int d = 0 ; d < 8 ; ++d ) {
+			int mr = row + dr[d];
+			int mc = col + dc[d];
+			if ( mr < 0 || mr >= h || mc < 0 || mc >= w ) continue;
+			if ( map[mr][mc] == 0 ) continue;
+			map[mr][mc] = 0;
+			dfs(mr,mc);
 		}
 	}
+	
 }
