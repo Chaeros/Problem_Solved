1a8849b456b87bf32c657883d38f02a3c93fe97e
Chanho Park <chpark6851@koreatech.ac.kr>
2024-03-25 05:56:33 +0900

    [Silver IV] Title: 자리배정, Time: 180 ms, Memory: 17044 KB -BaekjoonHub

---
+++ 백준/Silver/10157. 자리배정/자리배정.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int C = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(br.readLine());
+
+        if (K > C * R) {
+            bw.write("0\n");
+            bw.flush();
+            bw.close();
+            return;
+        }
+
+        int dx[] = {-1, 0, 1, 0};
+        int dy[] = {0, 1, 0, -1};
+
+        boolean[][] visit = new boolean[R][C];
+
+        int cnt = 0, x = R - 1, y = 0, dir = 0;
+        while (++cnt != K) {
+            visit[x][y] = true;
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+
+            if (nx < 0 || ny < 0 || nx >= R || ny >= C || visit[nx][ny]) {
+                dir = ++dir % 4;
+                nx = x + dx[dir];
+                ny = y + dy[dir];
+            }
+
+            x = nx;
+            y = ny;
+        }
+        bw.write((y+1)+" "+(R-x));
+        bw.flush();
+        bw.close();
+    }
+}
