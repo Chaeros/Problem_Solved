ba47db6d9da9cfeae858459adce034838abd0747
Chanho Park <chpark6851@koreatech.ac.kr>
2024-04-11 15:26:53 +0900

    [Gold II] Title: 저울, Time: 164 ms, Memory: 16740 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		int arr[] = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for ( int n = 0 ; n < N ; ++n ) {
+			arr[n] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		int count = 1;
+		int sum = 0;
+		int index = 0;
+		while(true) {
+			if ( sum < count ) {
+				if ( index == N ) break;
+				if ( arr[index] > count ) {
+					break;
+				}
+				sum += arr[index++];
+			}
+			++count;
+		}
+		bw.write(count+"\n");
+		bw.flush();
+		bw.close();
+	}
+}
