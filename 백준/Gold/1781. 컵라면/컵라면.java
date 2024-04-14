56ff297278007c120055d1e6fc3265ee99566860
Chanho Park <chpark6851@koreatech.ac.kr>
2024-04-14 19:10:06 +0900

    [Gold II] Title: 컵라면, Time: 1200 ms, Memory: 101096 KB -BaekjoonHub

---
+++ 백준/Gold/1781. 컵라면/컵라면.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		List<Integer>[] list = new LinkedList[N+1];
+		for ( int i = 0 ; i < N+1 ; ++i ) {
+			list[i] = new LinkedList<>();
+		}
+		
+		TreeSet<Integer> set = new TreeSet<Integer>();
+		for ( int n = 0 ; n < N ; ++n ) {
+			st = new StringTokenizer(br.readLine());
+			int deadline = Integer.parseInt(st.nextToken());
+			int count = Integer.parseInt(st.nextToken());
+			list[deadline].add(count);
+			set.add(deadline);
+		}
+		
+		for ( int n = 1 ; n <= N ; ++n ) {
+			Collections.sort(list[n],(o1,o2)->o2-o1);
+		}
+		
+		int resultSum = 0;
+		PriorityQueue<Integer> pQ = new PriorityQueue<>((o1,o2)->o2-o1);
+		for ( int t = N ; t >= 1 ; --t ) {
+			if ( set.contains(t) ) {
+				for ( int x : list[t] ) {
+					pQ.add(x);
+				}
+			}
+			if ( !pQ.isEmpty() ) {
+				resultSum += pQ.poll();
+			}
+		}
+		System.out.println(resultSum);
+	}
+}
