e4452c49b7b7d0fb1d4dca92a9e2018d314aef67
Chanho Park <chpark6851@koreatech.ac.kr>
2024-04-15 23:54:52 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 125 ms, Memory: 35,364 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,71 +1,61 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Solution {
-	
-	static int N,M;
-	static int parents[];
-	
-	public static void main(String[] args) throws IOException {
+class Solution
+{
+    public static int N,M;
+    public static boolean visited[];
+    public static ArrayList<ArrayList<Integer>> list;
+    
+    public static void dfs(int x){
+        for(int val:list.get(x)){
+            if(!visited[val]){
+                visited[val]=true;
+                dfs(val);
+            }
+        }
+    }
+    
+	public static void main(String args[]) throws Exception
+	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st; 
-		
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+               
 		int T = Integer.parseInt(br.readLine());
-		
-		for ( int t = 1 ; t <= T ; ++t ) {
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			M = Integer.parseInt(st.nextToken());
-			parents = new int[N+1];
-			makeParent();
-			Set<Integer> set = new HashSet<>();
-			for ( int i = 0 ; i < M ; ++i ) {
-				st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				union(a, b);
-			}
-			for ( int i = 1 ; i <= N ; ++i ) {
-				set.add(find(i));
-			}
-			sb.append("#"+t+" "+set.size()+"\n");
-		}
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
 
-	private static void makeParent() {
-		for ( int i = 1 ; i <= N ; ++i ) {
-			parents[i] = i;
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            list = new ArrayList<>();
+			st = new StringTokenizer(br.readLine());
+            N =Integer.parseInt(st.nextToken());
+            M =Integer.parseInt(st.nextToken());
+            visited = new boolean[N+1];
+            
+            for(int i=0;i<=N;++i){
+             	list.add(new ArrayList<>());   
+            }
+            
+            for(int i=0;i<M;++i){
+                list.add(new ArrayList<>());
+             	st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                list.get(a).add(b);
+                list.get(b).add(a);
+            }
+            
+            int result=0;
+            for(int i=1;i<=N;++i){
+                if(!visited[i]){
+                    dfs(i);
+                    result++;
+                }
+            }
+            
+            bw.write("#"+test_case+" "+result+"\n");            
 		}
+        bw.flush();
+        bw.close();
 	}
-	
-	private static int find(int x) {
-		if ( x == parents[x] ) return x;
-		return parents[x] = find(parents[x]);
-	}
-	
-	private static void union(int a, int b) {
-		int aRoot = find(a);
-		int bRoot = find(b);
-		
-		if ( aRoot == bRoot ) return;
-		if ( aRoot < bRoot ) {
-			parents[bRoot] = aRoot;
-		}
-		else {
-			parents[aRoot] = bRoot;
-		}
-	}
-	
-	
 }
